e50f2bbc5f0b2d905410231d0e1fac9964bf50fc
Summer Sheldon <ssheldon@umd.edu>
2023-05-11 14:25:52 -0400

    switch to hashrouter for gh-pages

--- src/index.js
+++ src/index.js
@@ -1,34 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Route,
     Routes,
     Navigate,
-  } from "react-router-dom";
+} from "react-router-dom";
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import App from "./App";
 
 function getRndInteger(min, max) {
-    const val = Math.floor(Math.random() * (max - min) ) + min;
+    const val = Math.floor(Math.random() * (max - min)) + min;
     return val;
 }
 
-const gameContainer = document.getElementById('wordle');
+const gameContainer = document.getElementById("wordle");
 const gameRoot = ReactDOM.createRoot(gameContainer);
 gameRoot.render(
     <Router>
-    <Routes>
-        <Route 
-        path="/"
-        element={<Navigate replace to={`/4/${getRndInteger(1, 65536)}`}/>}
-        />
-        <Route
-        path="/:num_games/:seed"
-        element={<App />}
-        />
-    </Routes>
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <Navigate replace to={`/4/${getRndInteger(1, 65536)}`} />
+                }
+            />
+            <Route path="/:num_games/:seed" element={<App />} />
+        </Routes>
     </Router>
 );
-
